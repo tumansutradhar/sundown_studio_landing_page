af995799d774bca84c4a368ad02726018c439840
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-02-26 18:19:08 +0530

    complete project section

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var projectContainer = document.querySelector("#project-container");
+var projectImages = document.querySelector("#project-images");
+
+projectContainer.addEventListener("mouseenter", function () {
+    projectImages.style.display = "block";
+});
+
+projectContainer.addEventListener("mouseleave", function () {
+    projectImages.style.display = "none";
+});
+
+var projects = document.querySelectorAll("#projects");
+
+projects.forEach(function (a) {
+    a.addEventListener("mouseenter", function () {
+        var dataImages = a.getAttribute("data-images");
+        projectImages.style.backgroundImage = `url(${dataImages})`;
+    });
+});
