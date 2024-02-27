66746924a59171e2b96acf766ae84fe61357aaed
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-02-27 22:14:13 +0530

    add page 4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
 
 var projectContainer = document.querySelector("#project-container");
 var projectImages = document.querySelector("#project-images");
