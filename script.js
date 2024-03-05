1cfee577a3425b258052117b45a6d1a35a40a7bb
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-03-05 17:29:03 +0530

    hamburger menu animation add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Page 3 Section Start */
+// Page 3 Section Start
 
 function projectHover() {
     var featuredContainer = document.querySelector("#featured-container");
@@ -24,9 +24,9 @@ function projectHover() {
 
 projectHover();
 
-/* Page 3 Section End */
+// Page 3 Section End
 
-/* Page 5 Section Start */
+// Page 5 Section Start
 
 function swipeHorizontal() {
     var swiper = new Swiper(".mySwiper", {
@@ -38,4 +38,29 @@ function swipeHorizontal() {
 
 swipeHorizontal();
 
-/* Page 5 Section End */
+// Page 5 Section End
+
+// Menu Animation Section Start
+
+function menuAnimation() {
+    var menu = document.querySelector("nav>h4");
+    var menuClick = document.querySelector("#menu-click");
+    var navImg = document.querySelector("nav img");
+    var flag = 0;
+
+    menu.addEventListener("click", function () {
+        if (flag == 0) {
+            menuClick.style.top = 0;
+            navImg.style.opacity = 0;
+            flag = 1;
+        } else {
+            menuClick.style.top = "-100%";
+            navImg.style.opacity = 1;
+            flag = 0;
+        }
+    });
+}
+
+menuAnimation();
+
+// Menu Animation Section End
